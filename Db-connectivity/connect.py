a7c08bf6dd5bc252aa2c3f8a905babbac5bf2df0
noorulain <89151770+noorul-ain@users.noreply.github.com>
2024-06-04 20:15:05 +0500

    db

---
+++ Db-connectivity/connect.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv 
+import os 
+from sqlalchemy import create_engine,text
+
+
+load_dotenv()
+key = os.getenv("db_key")
+# print(key)
+
+
+
+conn_str = f'{key}'
+engine = create_engine(conn_str)
+
+
